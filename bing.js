b9ce80f50a5b3be65e6285017e3a07796717993c
Tim Cameron Ryan <id@timryan.org>
2012-11-20 18:29:05 -0500

    Spaces

--- bing.js
+++ bing.js
@@ -1,34 +1,34 @@
 var rem = require('rem');
 
 var api = rem.create({
-	base: 'http://api.microsofttranslator.com/v2',
-	configParams: {
-		appId: 'key'
-	}
+  base: 'http://api.microsofttranslator.com/v2',
+  configParams: {
+    appId: 'key'
+  }
 }, {
-	key: "T9zub_9uszOuS8dZiC8mpVBGTWtgp4Nb72eQazRmAmU4*"
+  key: "T9zub_9uszOuS8dZiC8mpVBGTWtgp4Nb72eQazRmAmU4*"
 });
 
 exports.translate = function (from, to, text, next) {
-	api('ajax.svc/TranslateArray').get({
-		texts: JSON.stringify([text]),
-		from: JSON.stringify(from),
-		to: JSON.stringify(to),
-		loc: 'en',
-		ctr: 'UnitedStates',
-		rgp: 'c436032'
-	}, function (err, json) {
-		next(err, json && json.map(function (translation) {
-			return translation.TranslatedText;
-		}));
-	});
+  api('ajax.svc/TranslateArray').get({
+    texts: JSON.stringify([text]),
+    from: JSON.stringify(from),
+    to: JSON.stringify(to),
+    loc: 'en',
+    ctr: 'UnitedStates',
+    rgp: 'c436032'
+  }, function (err, json) {
+    next(err, json && json.map(function (translation) {
+      return translation.TranslatedText;
+    }));
+  });
 };
 
 exports.pronounce = function (lang, text, next) {
-	api.stream('http.svc/Speak').get({
-		language: lang,
-		format: 'audio/mp3',
-		options: 'MaxQuality',
-		text: text
-	}, next);
+  api.stream('http.svc/Speak').get({
+    language: lang,
+    format: 'audio/mp3',
+    options: 'MaxQuality',
+    text: text
+  }, next);
 };
--- google.js
+++ google.js
@@ -1,34 +1,34 @@
 var rem = require('rem');
 
 var api = rem.create({
-	base: 'http://translate.google.com',
-	configParams: {
-		client: 'key'
-	}
+  base: 'http://translate.google.com',
+  configParams: {
+    client: 'key'
+  }
 }, {
-	key: 'x'
+  key: 'x'
 });
 
 exports.translate = function (from, to, text, next) {
-	api('translate_a/t').get({
-		text: text,
-		hl: 'en',
-		sl: from,
-		tl: to
-	}, function (err, json) {
-		next(err, json && json.sentences.map(function (sentence) {
-			return sentence.trans;
-		}))
-	});
+  api('translate_a/t').get({
+    text: text,
+    hl: 'en',
+    sl: from,
+    tl: to
+  }, function (err, json) {
+    next(err, json && json.sentences.map(function (sentence) {
+      return sentence.trans;
+    }))
+  });
 };
 
 exports.pronounce = function (lang, text, next) {
-	api.stream('translate_tts').get({
-		ie: 'UTF-8',
-		q: text,
-		tl: lang,
-		total: text.split(/\s+/).length,
-		idx: 0,
-		textlen: text.length
-	}, next);
+  api.stream('translate_tts').get({
+    ie: 'UTF-8',
+    q: text,
+    tl: lang,
+    total: text.split(/\s+/).length,
+    idx: 0,
+    textlen: text.length
+  }, next);
 };
