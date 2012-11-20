29de322bdd62de1a358b66d6cc2634f8b07ca6f0
Tim Cameron Ryan <id@timryan.org>
2012-11-20 17:59:59 -0500

    Support bing, bump Rem version

--- bing.js
+++ bing.js
@@ -1,34 +1,34 @@
 var rem = require('rem');
 
 var api = rem.create({
-	base: 'http://translate.google.com',
+	base: 'http://api.microsofttranslator.com/v2',
 	configParams: {
-		client: 'key'
+		appId: 'key'
 	}
 }, {
-	key: 'x'
+	key: "T9zub_9uszOuS8dZiC8mpVBGTWtgp4Nb72eQazRmAmU4*"
 });
 
 exports.translate = function (from, to, text, next) {
-	api('translate_a/t').get({
-		text: text,
-		hl: 'en',
-		sl: from,
-		tl: to
+	api('ajax.svc/TranslateArray').get({
+		texts: JSON.stringify([text]),
+		from: JSON.stringify(from),
+		to: JSON.stringify(to),
+		loc: 'en',
+		ctr: 'UnitedStates',
+		rgp: 'c436032'
 	}, function (err, json) {
-		next(err, json && json.sentences.map(function (sentence) {
-			return sentence.trans;
-		}))
+		next(err, json && json.map(function (translation) {
+			return translation.TranslatedText;
+		}));
 	});
 };
 
 exports.pronounce = function (lang, text, next) {
-	api.stream('translate_tts').get({
-		ie: 'UTF-8',
-		q: text,
-		tl: lang,
-		total: text.split(/\s+/).length,
-		idx: 0,
-		textlen: text.length
+	api.stream('http.svc/Speak').get({
+		language: lang,
+		format: 'audio/mp3',
+		options: 'MaxQuality',
+		text: text
 	}, next);
 };
